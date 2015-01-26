3f33c1d53bedf2d93050bc8909d1f648096d3788
Şenol Alan <palynology@gmail.com>
2015-01-26 09:42:19 +0200

    Some cleanup

--- monitor.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-until main.py; do
-    echo "'myscript.py' crashed with exit code $?. Restarting..." >&2
-    sleep 1
-done
--- tem.py
+++
@@ -1,5 +0,0 @@
-from dht22 import humtemp
-from baro import bastemp
-
-print humtemp()
-print bastemp()
